1b492dca9a16e0a6ce67ae49f26144b328c1ff0e
silence <silence.zpk@gmail.com>
2025-08-30 14:32:06 +0800

    fix: i18n.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
     // A list of all locales that are supported
-    locales: ['en-us'],
+    locales: ['en-us', 'zh-cn', 'de-de', 'en-es', 'fr-fr', 'it-it', 'ja-jp', 'ko-kr', 'pt-pt', 'ro-ro', 'ru-ru'],
     localePrefix: 'as-needed',
     // Used when no locale matches
     defaultLocale: 'en-us'
